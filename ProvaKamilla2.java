7450e68be5b93219e9786ee5a103e0318cc61838
RafaelPassosS <128323662+RafaelPassosS@users.noreply.github.com>
2023-06-15 19:36:30 -0300

    Add files via upload

---
+++ ProvaKamilla2.java
@@ -0,0 +1,21 @@
+package prova.kamilla.pkg2;
+
+
+public class ProvaKamilla2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String opc = "10";
+        while(opc != "0"){
+            System.out.print("1- Cadastrar Clientes");
+            System.out.print("2 - Cadastrar Vendedor");
+            System.out.print("3 - Cadastrar Produto");
+            System.out.print("4 - Vender Produto");
+            System.out.print("5 - Relatorio de Vendas");
+        }
+        
+    }
+    
+}
